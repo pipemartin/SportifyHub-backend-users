e9e02cadf2cf98a2e682d1bad4fec29000fef335
pipemartin <juanfelipem09@hotmail.com>
2023-11-14 10:47:52 -0500

    CustomTokenObtain

--- users/api/views.py
+++ users/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.api.serializers import UserRegisterSerializers, UserSerializer, UserUpdateSerializer
 from users.models import User
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class RegisterView(APIView):
@@ -34,3 +36,21 @@ class UserView(APIView):
             return Response(serializer.data)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, User):
+        token = super().get_token(User)
+
+        rol = User.rol
+
+        token['username'] = User.username
+        token['user_id'] = User.id
+        token['first_name'] = User.first_name
+        token['rol_title'] = rol.title
+        return token
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
