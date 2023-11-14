b93e57a7330b42fe0292403b0ede0a21a9f53d2a
pipemartin <juanfelipem09@hotmail.com>
2023-11-14 10:54:56 -0500

    UPDATE USER ROUTER

--- users/api/router.py
+++ users/api/router.py
@@ -1,11 +1,12 @@
 from django.urls import path
+
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-from users.api.views import RegisterView, UserView
+from users.api.views import RegisterView, UserView, CustomTokenObtainPairView
 
 urlpatterns = [
     path('auth/register', RegisterView.as_view()),
     # users api
-    path('auth/login', TokenObtainPairView.as_view()),
+    path('auth/login', CustomTokenObtainPairView.as_view()),
     path('auth/token/refresh', TokenRefreshView.as_view()),
     # Obteniendo los datos del usuario logeado:
     path('auth/me', UserView.as_view()),
